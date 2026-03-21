fe178fb6c565ce6c606624423974dd0a91447dd8
aezeshia <profakihiko02@gmail.com>
2026-03-21 18:32:24 +0800

    Delete start_streamlit_8501.cmd

--- start_streamlit_8501.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-start "Hugyoku8501" /min "C:\Users\AE_Ze\PycharmProjects\Hugyoku\.venv\Scripts\python.exe" -m streamlit run "C:\Users\AE_Ze\PycharmProjects\Hugyoku\streamlit_app.py" --server.headless true --server.port 8501
-exit /b 0
